e97473530accda30b5604821769e727a83855c76
Shehara Silva <sheharasilva24@gmail.com>
2025-05-01 12:15:24 +0530

    Added grounded state detection using CapsuleCollider2D cast and updated Animator parameter via AnimationStrings

--- Assets/Scripts/AnimationStrings.cs
+++ Assets/Scripts/AnimationStrings.cs
@@ -8,4 +8,5 @@ internal class AnimationStrings
 {
     internal static string isMoving = "isMoving";
     internal static string isRunning = "isRunning";
+    internal static string isGrounded = "isGrounded";
 }

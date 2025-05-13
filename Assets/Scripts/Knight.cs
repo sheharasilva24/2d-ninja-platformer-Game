cf690c874c4b5da91ce2a96445d7e8dd526ceac6
Shehara Silva <sheharasilva24@gmail.com>
2025-05-14 01:13:49 +0530

    Add attack cooldown timer and cliff-based direction flip

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -12,6 +12,7 @@ public class Knight : MonoBehaviour
     public float walkSpeed = 3f;
     public float walkStopRate = 0.05f;
     public DetectionZone attackZone;
+    public DetectionZone cliffDetectionZone;
 
     Rigidbody2D rb;
     TouchingDirections touchingDirections;
@@ -89,6 +90,12 @@ public class Knight : MonoBehaviour
         } 
     }
 
+    public float AttackCooldown { get {
+            return animator.GetFloat(AnimationStrings.attackCooldown);
+        } private set { 
+            animator.SetFloat(AnimationStrings.attackCooldown, Mathf.Max(value, 0));
+        } }
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -100,6 +107,11 @@ public class Knight : MonoBehaviour
     void Update()
     {
         HasTarget = attackZone.detectedColliders.Count > 0;
+
+        if(AttackCooldown > 0)
+        {
+            AttackCooldown -= Time.deltaTime;
+        }
     }
 
     private void FixedUpdate()
@@ -141,6 +153,14 @@ public class Knight : MonoBehaviour
     public void OnHit(int damage, Vector2 knockback)
     {
         rb.velocity = new Vector2(knockback.x, rb.velocity.y + knockback.y);
+    }
+
+    public void OnCliffDetected ()
+    {
+        if (touchingDirections.IsGrounded)
+        {
+            FlipDirection();
+        }
     }
 
 }

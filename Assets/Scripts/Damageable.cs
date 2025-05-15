79deeb04c37e55065fc5ca0cb2ed4acf2df1cfb7
Shehara Silva <sheharasilva24@gmail.com>
2025-05-16 00:43:35 +0530

    updated

--- Assets/Scripts/Damageable.cs
+++ Assets/Scripts/Damageable.cs
@@ -119,15 +119,17 @@ public class Damageable : MonoBehaviour
         return false;
     }
 
-    public void Heal(int healthRestore) 
+    public bool Heal(int healthRestore) 
     {
-        if (IsAlive)
+        if (IsAlive && Health < MaxHealth)
         {
             int maxHeal = Mathf.Max(MaxHealth - Health, 0);
             int actualHeal = Mathf.Min(maxHeal, healthRestore);
             Health += actualHeal;
             CharacterEvents.characterHealed(gameObject, actualHeal);
+            return true;
         }
+        return false;
     }
 
 }
